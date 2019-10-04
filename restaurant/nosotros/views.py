3baef877afbf8c5d98eb7437dd5d8088c4b5b224
unknown <fjsszx@gmail.com>
2019-10-04 18:54:08 -0500

    Creacion del apartado de nosotros

--- restaurant/nosotros/views.py
+++ restaurant/nosotros/views.py
@@ -1,3 +1,13 @@
+from usuarios.forms import UserForm, UserAdminForm, InfoExtraUsuarioForm
 from django.shortcuts import render
+from django.views.generic import View, TemplateView, ListView, DetailView, FormView
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from . import models
 
 # Create your views here.
+
+class nosotosTemplateView(TemplateView):
+		template_name = 'nosotros/contact.html'
--- restaurant/principal/urls.py
+++ restaurant/principal/urls.py
@@ -1,7 +1,7 @@
-"""restaurant URL Configuration
+"""papeleria URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from principal import views
 
+app_name = 'principal'
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.IndexView.as_view(), name="index"),
-    path('usuarios/', include('usuarios.urls')),
-    path('principal/', include('principal.urls')),
-    path('nosotros/', include('nosotros.urls')),
 ]
